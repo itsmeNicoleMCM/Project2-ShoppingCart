9298b6b45b823bdc0e38089185a8afd243b040bf
itsmenicolemae <itsmenicolem.c.m.015@gmail.com>
2021-04-30 22:46:17 +0800

    Print from storage on load

--- js/app.js
+++ js/app.js
@@ -16,6 +16,9 @@ function loadEventListeners() {
     //Clear Cart Btn
     clearCartBtn.addEventListener('click', clearCart);
 
+    //Document Ready
+    document.addEventListener('DOMContentLoaded', getFromLocalStorage);
+
 }
 
 // Functions
@@ -115,4 +118,31 @@ function clearCart() {
     while(shoppingCartContent.firstChild) {
         shoppingCartContent.removeChild(shoppingCartContent.firstChild);
     }
+}
+
+//Loads when document is ready and print courses into shopping cart
+
+function getFromLocalStorage() {
+    let coursesLS = getCoursesFromStorage();
+
+    //Loop throught the courses and print into the cart
+    coursesLS.forEach(function (course) {
+            // create the <tr>
+            const row = document.createElement('tr');
+
+            //print the content 
+            row.innerHTML = `
+            <tr>
+                <td>
+                    <img src = "${course.image}" width=100>
+                </td>
+                <td> ${course.title}</td>
+                <td>${course.price}</td>
+                <td>
+                    <a href="#" class="remove" data-id="${course.id}">X</a>
+                </td>
+            </tr>  
+        `;
+        shoppingCartContent.appendChild(row);
+    });
 }
