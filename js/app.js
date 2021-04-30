ca4b9c4900e9c7325921166b814d9e404841c9d2
itsmenicolemae <itsmenicolem.c.m.015@gmail.com>
2021-04-30 22:43:46 +0800

    Removing courses in the cart and clear the cart

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 // Variables
 const courses = document.querySelector('#courses-list'),
-      shoppingCartContent = document.querySelector('#cart-content tbody');
+shoppingCartContent = document.querySelector('#cart-content tbody'),
+clearCartBtn = document.querySelector('#clear-cart');
 
 //Listeners
 loadEventListeners();
@@ -8,6 +9,12 @@ loadEventListeners();
 function loadEventListeners() {
     //When a new course is added
     courses.addEventListener('click', buyCourse);
+
+    //When the remove button is clicked
+    shoppingCartContent.addEventListener('click', removeCourse);
+
+    //Clear Cart Btn
+    clearCartBtn.addEventListener('click', clearCart);
 
 }
 
@@ -57,9 +64,26 @@ function addIntoCart(course) {
             <td>
                 <a href="#" class="remove" data-id="${course.id}">X</a>
             </td>
-        </tr>
+        </tr>  
     `;
 
-    //Add into the shopping cart
-    shoppingCartContent.appendChild(row);
+        //Add into the shopping cart
+        shoppingCartContent.appendChild(row);
+}
+
+//Remove course from the DOM
+function removeCourse(e) {
+
+    if(e.target.classList.contains('remove')) {
+        e.target.parentElement.parentElement.remove();
+
+    }
+}
+//clears the shopping cart
+function clearCart() {
+    //shoppingCartContent.innerHTML = '';
+
+    while(shoppingCartContent.firstChild) {
+        shoppingCartContent.removeChild(shoppingCartContent.firstChild);
+    }
 }
